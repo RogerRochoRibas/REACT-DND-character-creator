2e717129306ed59c715b40da163308686bfd6fca
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-07-03 17:19:08 +0200

    Deleted the character sheet provider

--- src/pages/characterSheet.js
+++ src/pages/characterSheet.js
@@ -1,14 +1,11 @@
 import CharacterSheetContent from "../components/characterSheet/characterSheetContent";
-import CharacterSheetProvider from "../components/characterSheet/characterSheetProvider";
 import ChangePage from "../components/changePage";
 
 const CharacterSheet = () => {
   return (
     <div>
-      <CharacterSheetProvider>
         <ChangePage />
         <CharacterSheetContent />
-      </CharacterSheetProvider>
     </div>
   );
 };
