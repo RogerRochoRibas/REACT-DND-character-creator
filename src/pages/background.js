6a10e8099bbab5da9d966b71ce463009a745ad5c
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-10-27 21:19:14 +0200

    eliminate bottom navigation if not necessary

--- src/pages/background.js
+++ src/pages/background.js
@@ -8,7 +8,6 @@ const Background = () => {
         <ChangePage />
         <BackgroundSelector />
         <BackgroundShowcase />
-        <ChangePage />
     </div>
   );
 };
--- src/pages/stats.js
+++ src/pages/stats.js
@@ -14,7 +14,6 @@ const Stats = () => {
         <RollForstats />
         <PointBuy />
         <SetManually />
-        <ChangePage />
     </div>
   );
 };
